1b0ce676b07823d3d4f13b9f9f897fbdfa661a85
Lisa Rolczynski <lisa.sano@yahoo.com>
2015-04-05 21:29:32 -0700

    adds user info and public photo info to a database using postgresql (psycopg2) whenever a new email address is searched for

--- FlickrAPIrequest.py
+++ FlickrAPIrequest.py
@@ -15,13 +15,14 @@ def get_request(my_params):
 	return r.json()
 
 
-def find_member_id(email_address):
+def find_member_id_and_username(email_address):
 	'''find a member using their email address. Print out the json response and user id number'''
 	findMemberWithEmail = {'method': 'flickr.people.findByEmail', 'find_email': email_address}
 	request1 = get_request(findMemberWithEmail)	
-	print request1
 	userIDNumber = request1['user']['nsid']   #nested dict call
-	return userIDNumber
+	username = request1['user']['username']['_content']
+	user_info_dict = {'user_id': userIDNumber, 'username': username}
+	return user_info_dict
 
 
 def get_favorites_list(userIDNumber):
@@ -37,6 +38,7 @@ def get_public_photos(userIDNumber):
 	publicPhotos = {'method': 'flickr.people.getPublicPhotos', 'user_id': userIDNumber, 'per_page': 5, 'page': 1}
 	request3 = get_request(publicPhotos)
 	photo_list = request3['photos']['photo']
+	# photo_list_dict = {'photo_id': photo_list['id'], 'photo_title': photo_list['title']}
 	return photo_list
 
 
@@ -44,4 +46,7 @@ def find_user_profile(userIDNumber):
 	'''get user profile for the person whose email was searched for'''
 	findUserProfile = {'method': 'flickr.urls.getUserProfile', 'user_id': userIDNumber}
 	request4 = get_request(findUserProfile)
-	return request4
+	my_url = request4['user']['url']
+	return my_url
+
+	# output: {u'stat': u'ok', u'user': {u'url': u'https://www.flickr.com/people/61965480@N03/', u'nsid': u'61965480@N03'}}
--- FlickrUser.py
+++ FlickrUser.py
@@ -1,115 +1,118 @@
 from FlickrAPIrequest import *
 from FlickrPhoto import Photo
-
+import psycopg2
 
 class User(object):
-	
-	def __init__(self, email_address):
-		self.email = email_address
-		
-		self.user_id = find_member_id(self.email)
-		self.pub_photos = []
-		self.fave_list = []
-		self._set_public_photos()
-		self._set_fave_list()
-		
-	def _make_photo_instance_from_raw_data(self, raw_photo_data):
-		'''creates and returns an instance of class photo'''
-		photo_title = raw_photo_data['title']
-		photo_id = raw_photo_data['id']
-		photo_instance = Photo(photo_title, photo_id)
-		return photo_instance
-		
-		
-	def _set_public_photos(self):
-		'''creates and prints a list of public photos'''
-		raw_photo_data = get_public_photos(self.user_id)
-		for photo in raw_photo_data:
-			new_one = self._make_photo_instance_from_raw_data(photo)
-			self.pub_photos.append(new_one)
-		print self.pub_photos
-		
-		
-		
-	def _set_fave_list(self):
-		'''creates and print a list of favorite photos'''
-		raw_fave_list = get_favorites_list(self.user_id)
-		for photo in raw_fave_list:
-			new_fave = self._make_photo_instance_from_raw_data(photo)
-			self.fave_list.append(new_fave)
-		print self.fave_list
+    
+    def __init__(self, email_address):
+        self.email = email_address
+        
+        self.user_id = find_member_id_and_username(self.email)['user_id']
+        print "USER_ID IS: %s" % self.user_id
+        self.username = find_member_id_and_username(self.email)['username']
+        print "USERNAME IS: %s" % self.username
+        self.url = find_user_profile(self.user_id)
+        print "URL IS: %s" % self.url
+        self.pub_photos = []
+        self.fave_list = []
+        self._set_public_photos()
+        self._set_fave_list()
+        self._save_user_info()
+        self._save_public_photo_info()
+        
+    def _make_photo_instance_from_raw_data(self, raw_photo_data):
+        '''creates and returns an instance of class photo'''
+        photo_title = raw_photo_data['title']
+        photo_id = raw_photo_data['id']
+        photo_instance = Photo(photo_title, photo_id)
+        return photo_instance
+
+    def _set_public_photos(self):
+        '''creates and prints a list of public photos'''
+        raw_photo_data = get_public_photos(self.user_id)
+        for photo in raw_photo_data:
+            new_one = self._make_photo_instance_from_raw_data(photo)
+            self.pub_photos.append(new_one)
+        print "PUBLIC PHOTOS are: %s" % self.pub_photos
 
 
+    def _set_fave_list(self):
+        '''creates and print a list of favorite photos'''
+        raw_fave_list = get_favorites_list(self.user_id)
+        for photo in raw_fave_list:
+            new_fave = self._make_photo_instance_from_raw_data(photo)
+            self.fave_list.append(new_fave)
+        print "FAVORITE PHOTOS are: %s" % self.fave_list
+
+    def _save_user_info(self):
+        conn = psycopg2.connect("dbname=lisa user=lisa")
+        cur = conn.cursor()
+        cur.execute(
+            '''
+                DO
+                $do$
+                BEGIN
+                if exists (SELECT * from flickr_user_info 
+                WHERE user_id = %s) THEN
+                    UPDATE flickr_user_info
+                        SET username = %s
+                        WHERE user_id = %s;
+                ELSE
+                    INSERT INTO flickr_user_info
+                        (user_id, username, url)
+                    VALUES
+                        (%s, %s, %s);
+                END IF;
+                END
+                $do$
+            ''',
+            (self.user_id, self.username, self.user_id, self.user_id, self.username, self.url)
+        )
+
+    
+        cur.execute("SELECT * FROM flickr_user_info;")
+        fetch_user_info = cur.fetchall()
+        print fetch_user_info
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
 
 
-'''
+    def _save_public_photo_info(self):
+        conn = psycopg2.connect("dbname=lisa user=lisa")
+        cur = conn.cursor()
 
-photos = [
-	{blahblah},
-	{blahblah}
-]
-{
-    u'photos': {
-        u'total': u'2',
-        u'photo': [
-            {
-                u'isfamily': 0,
-                u'title': u'SunsetfromTitlowBeach',
-                u'farm': 8,
-                u'ispublic': 1,
-                u'server': u'7287',
-                u'isfriend': 0,
-                u'secret': u'83ce59a669',
-                u'owner': u'61965480@N03',
-                u'id': u'16555365651'
-            },
-            {
-                u'isfamily': 0,
-                u'title': u'SunsetfromTitlowBeach',
-                u'farm': 8,
-                u'ispublic': 1,
-                u'server': u'7457',
-                u'isfriend': 0,
-                u'secret': u'ed1eb629fc',
-                u'owner': u'61965480@N03',
-                u'id': u'16369591050'
-            }
-        ],
-        u'perpage': 5,
-        u'page': 1,
-        u'pages': 1
-    },
-    u'stat': u'ok'
-}u'photos': {
-    u'total': u'2',
-    u'photo': [
-        {
-            u'isfamily': 0,
-            u'title': u'SunsetfromTitlowBeach',
-            u'farm': 8,
-            u'ispublic': 1,
-            u'server': u'7287',
-            u'isfriend': 0,
-            u'secret': u'83ce59a669',
-            u'owner': u'61965480@N03',
-            u'id': u'16555365651'
-        },
-        {
-            u'isfamily': 0,
-            u'title': u'SunsetfromTitlowBeach',
-            u'farm': 8,
-            u'ispublic': 1,
-            u'server': u'7457',
-            u'isfriend': 0,
-            u'secret': u'ed1eb629fc',
-            u'owner': u'61965480@N03',
-            u'id': u'16369591050'
-        }
-    ],
-    u'perpage': 5,
-    u'page': 1,
-    u'pages': 1
-},
-u'stat': u'ok'
-}
-'''
+        for photo in self.pub_photos:
+            cur.execute(
+                '''
+                    DO
+                    $do$
+                    BEGIN
+                    if exists (SELECT * from flickr_public_photos
+                    WHERE photo_id = %s) THEN
+                        UPDATE flickr_public_photos
+                            SET title = %s
+                            WHERE photo_id = %s;
+                    ELSE
+                        INSERT INTO flickr_public_photos
+                            (user_id, photo_id, title)
+                        VALUES
+                            (%s, %s, %s);
+                    END IF;
+                    END
+                    $do$
+                ''',
+                (photo.id, photo.title, photo.id, self.user_id, photo.id, photo.title)
+
+            )
+
+            cur.execute("SELECT * from flickr_public_photos;")
+            fetch_pub_photo_info = cur.fetchall()
+            print fetch_pub_photo_info
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
