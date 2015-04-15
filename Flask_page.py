dc4cece79e507b4012614729bd284b0b8113218e
Lisa Rolczynski <lisa.sano@yahoo.com>
2015-04-14 22:34:00 -0700

    searching an email address returns a user_id

--- Flask_page.py
+++ Flask_page.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import FlickrAPIrequest
+
+from FlickrUser import User
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -7,9 +10,12 @@ app.debug = True
 def flickr():
     return render_template('main_form.html')
 
-@app.route('/lookup.html')
+@app.route('/lookup', methods=['POST'])
 def lookup_route():
-    return "hi"
+    email_address = request.form.get('email')
+    user1 = User(email_address)
+    user1.fetch_user_info()
+    return user1.user_id
 
 if __name__ == '__main__':
     app.run()
