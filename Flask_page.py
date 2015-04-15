b64c6e4e773125d07f254d04c6fe5b4d3796bfb7
Lisa Rolczynski <lisa.sano@yahoo.com>
2015-04-14 21:32:15 -0700

    added an email prompt in the main route

--- Flask_page.py
+++ Flask_page.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def flickr():
-    return 'My Flickr page'
+    return render_template('main_form.html')
 
 if __name__ == '__main__':
     app.run()
